7fd8b8ec62274de9c4fc4c97fa58826894c95680
Aurel1701 <gantzfan96@gmail.com>
2022-04-20 14:55:35 +0200

    edit js

--- assets/app.js
+++ assets/app.js
@@ -1,5 +1,21 @@
-const app = new Vue (
+//Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+
+const app = new Vue(
     {
+        // cerco il div su cui lavorare
+        el: 'app',
+        
+
+        // creo un data da dove prendere l'input 
+
+        data: {
+            messaggio: 'Ciao Aurel'
+        },
 
 
-})
+    },
+);
+
+
+//Aggiungere alla pagina un’immagine, presa anch’essa da un data.
+//confermate lettura come al solito e buon divertimento 
